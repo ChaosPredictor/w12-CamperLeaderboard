c04516253dc0f1c254cdd4b0d0b5bc1e5636aa96
chaospredictor <kuzi81@gmail.com>
2017-03-10 19:19:52 +0200

    react-json-table added, table for example working

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,18 @@ require('normalize.css/normalize.css');
 require('styles/Main.css');
 
 import React from 'react';
+import JsonTable from 'react-json-table'
+var items = [
+  { name: 'Louise', age: 27, color: 'red' },
+  { name: 'Margaret', age: 15, color: 'blue'},
+  { name: 'Lisa', age:34, color: 'yellow'}
+];
 
 class MainComponent extends React.Component {
   render() {
     return (
       <div className="main">
-				main test
+				<JsonTable rows={ items } />
       </div>
     );
   }
