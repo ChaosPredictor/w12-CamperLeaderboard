36454a753b7309017e28b10665607197deac0bcf
chaospredictor <kuzi81@gmail.com>
2017-03-13 19:43:38 +0200

    image & username horizontal align

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,14 +6,22 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 var data = require('json!../data/alltime.json');
 
+function imageFormatter(cell, row){
+	return (<img style={{width:50}} src={cell}/>)
+}
+
+function imageName(cell, row) {
+	return (<div className="imageNameDiv"><img className="inDiv" style={{width:50}} src={row.img}/><p className="inDiv">{row.username}</p></div>) 
+}
+
 class MainComponent extends React.Component {
   render() {
     return (
       <div className="main">
 				<BootstrapTable data={data} striped hover condensed height='520' scrollTop={ 'Top' }>
-					<TableHeaderColumn dataField='username' isKey>Product ID</TableHeaderColumn>
-					<TableHeaderColumn dataField='alltime' dataSort={ true }>All time</TableHeaderColumn>
-					<TableHeaderColumn dataField='recent' dataSort={ true }>Last 30 days</TableHeaderColumn>
+		      <TableHeaderColumn dataField="img" dataFormat={imageName} isKey={true}>Camper user</TableHeaderColumn>
+					<TableHeaderColumn dataField='alltime' dataSort={ true }>All time points</TableHeaderColumn>
+					<TableHeaderColumn dataField='recent' dataSort={ true }>Points in Last 30 days</TableHeaderColumn>
 				</BootstrapTable>
       </div>
     );
