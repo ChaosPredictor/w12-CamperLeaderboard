6b3add13a289cde1936ea9a7423ea8f981299050
chaospredictor <kuzi81@gmail.com>
2017-03-14 12:50:19 +0200

    fixed index column added

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,8 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 var data = require('json!../data/alltime.json');
 
+
+
 function imageFormatter(cell, row){
 	return (<img style={{width:50}} src={cell}/>)
 }
@@ -14,11 +16,18 @@ function imageName(cell, row) {
 	return (<div className="imageNameDiv"><img className="inDiv" style={{width:50}} src={row.img}/><p className="inDiv">{row.username}</p></div>) 
 }
 
+
+function indexN(cell, row, enumObject, index) {
+	return (<div>{index+1}</div>) 
+}
+
 class MainComponent extends React.Component {
+
   render() {
     return (
       <div className="main">
 				<BootstrapTable data={data} striped hover condensed height='520' scrollTop={ 'Top' }>
+					<TableHeaderColumn className="indexColumn" width="30"  dataField="img" dataFormat={indexN}>#</TableHeaderColumn>
 		      <TableHeaderColumn dataField="img" dataFormat={imageName} isKey={true}>Camper user</TableHeaderColumn>
 					<TableHeaderColumn dataField='alltime' dataSort={ true }>All time points</TableHeaderColumn>
 					<TableHeaderColumn dataField='recent' dataSort={ true }>Points in Last 30 days</TableHeaderColumn>
