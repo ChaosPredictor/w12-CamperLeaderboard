87ab5e6858aac71d3751806ce53abc1cf4708e21
chaospredictor <kuzi81@gmail.com>
2017-03-21 23:14:44 +0200

    fix of main title

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,7 +7,7 @@ class TitleComponent extends React.Component {
   render() {
     return (
       <div>
-				<h1 className="title"> Title text </h1>
+				<h1 className="title"> Free Code Camper Leaderboard </h1>
       </div>
     );
   }
