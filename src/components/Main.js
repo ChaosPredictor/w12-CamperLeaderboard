633e1faf1a01a68fb42fef7652b63a3a9a0d5920
chaospredictor <kuzi81@gmail.com>
2017-03-21 20:31:14 +0200

    clean Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,23 +5,19 @@ import React from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import $ from 'jquery';
 
-function imageFormatter(cell, row){
-	return (<img style={{width:50}} src={cell}/>)
-}
+//function imageFormatter(cell, row){
+//	return (<img style={{width:50}} src={cell}/>)
+//}
 
 function imageName(cell, row) {
-	return (<div className="imageNameDiv"><img className="inDiv" style={{width:50}} src={row.img}/><p className="inDiv">{row.username}</p></div>) 
+	return (<div className="imageNameDiv"><img className="inDiv" style={{width:50}} src={row.img}/><p className="inDiv">{row.username}</p></div>)
 }
 
 function indexN(cell, row, enumObject, index) {
-	return (<div>{index+1}</div>) 
+	return (<div>{index+1}</div>)
 }
 
-function myFunction() {
-	alert ("for test")
-}
-
-var DATA = [{"username":"sjames1958gm","img":"https://avatars.githubusercontent.com/u/4639625?v=3","alltime":5810,"recent":384,"lastUpdate":"2017-03-11T03:22:18.809Z"}];
+var DATA = [{'username':'sjames1958gm','img':'https://avatars.githubusercontent.com/u/4639625?v=3','alltime':5810,'recent':384,'lastUpdate':'2017-03-11T03:22:18.809Z'}];
 
 class MainComponent extends React.Component {
 	
@@ -37,14 +33,14 @@ class MainComponent extends React.Component {
 		$.ajax({
 			url: 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime',
 			dataType: 'json',
-			success: function( jsonData ) {
-				console.log("load SUCCESS");
+			success: function( jsonData ){
+				console.log('load SUCCESS');
 				self.setState({
 					data: jsonData
 		    });
 			},
 			error: function(xhr, status, error) {
-				console.log("load ERROR");
+				console.log('load ERROR' + error);
 				//alert( "ERROR: " + xhr.responseText + "; Status:" + status + "; Error:" + error);
 			}
 			//timeout: 7000
@@ -58,7 +54,7 @@ class MainComponent extends React.Component {
 			this.setState({sortBy:sortName});
 			this.props.onSortChange(sortName);
 			//console.log("change");
-		};
+		}
 		this.setState({
 			sortName,
 			sortOrder
@@ -68,7 +64,7 @@ class MainComponent extends React.Component {
 	render() {
 		const options = {
 			sortName: this.state.sortName,
-			sortOrder: this.state.sortOrder, 
+			sortOrder: this.state.sortOrder,
 			onSortChange: this.onSortChange
 		};
     return (
